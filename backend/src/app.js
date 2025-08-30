87a8cd61e5987b2f0e703b4f082a23cb7eb5d105
Abhianv Kumar Mishra <mishrakabhianv34@gmail.com>
2025-08-30 17:16:34 +0530

    add url1

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import errorHandler from './middleware/errorHandler.middleware.js';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://vsbefe-spko.vercel.app',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
